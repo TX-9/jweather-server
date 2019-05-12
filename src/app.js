8c64ede4e592efc66353edf07d34332133f0a29f
TX-9 <jacobleecs@gmail.com>
2019-05-12 17:51:31 -0400

    add hbs partials

--- src/app.js
+++ src/app.js
@@ -1,15 +1,18 @@
 const path = require('path')
 const express = require('express')
+const hbs = require('hbs')
 
 const app = express()
 
 // Define paths for Express config
 const resourceDirectoryPath = path.join(__dirname, '../resource')
-const viewPath = path.join(__dirname, '../templates')
+const viewPath = path.join(__dirname, '../templates/views')
+const partialPath = path.join(__dirname, '../templates/partials')
 
 // Setup handlebars engine and views location
 app.set('view engine', 'hbs')
 app.set('views', viewPath)
+hbs.registerPartials(partialPath)
 
 // Set up static directory to serve
 app.use(express.static(resourceDirectoryPath))
@@ -17,24 +20,30 @@ app.use(express.static(resourceDirectoryPath))
 app.get('', (req, res) => {
     res.render('index', {
         title: 'Weather App',
+        author: 'Jacob',
         ver: '1.0'
     })
 })
 
 app.get('/about', (req, res) => {
     res.render('about', {
-        title: 'About'
+        title: 'About',
+        author: 'Jacob'
     })
 })
 
 app.get('/help', (req, res) => {
     res.render('help', {
-        desc: 'Help pages..'
+        title: 'Help',
+        author: 'Jacob',
+        helpTest: 'Help pages..'
     })
 })
 
 app.get('/weather', (req, res) => {
     res.send({
+        title: 'Weather',
+        author: 'Jacob',
         forecast: 'raining'
     })
 })
